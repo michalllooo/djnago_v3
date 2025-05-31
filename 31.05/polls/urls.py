2c85f826c8be9de8e239ab093c837c6f58d0a2da
Michal <hordziejewiczmichal@gmail.com>
2025-05-31 14:10:09 +0200

    popraawienie opisow

---
+++ 31.05/polls/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+    path('', views.index, name='index'),
+    path('warzywa/', views.warzywa, name='warzywa'),
+    path('ser/', views.ser, name='ser'),
+    path('pieczywo/', views.pieczywo, name='pieczywo'),
+    path('platnosci/', views.platnosci, name='platnosci'),
+]
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # djnago_v3
+
+run project : 
+
+put this command into a terminal in VS code : 
+ python manage.py runserver
+
+ final folder is : 31.05
